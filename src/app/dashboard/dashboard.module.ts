19f2c0cdec3d2d1e840a50ef11d12bb81c1be9c4
Simone <simone@localhost.localdomain>
2020-12-16 22:23:44 +0100

    use ngx-translate

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,21 +2,37 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { IonicModule, ModalController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
 import { StreakCountComponent } from './streak-count/streak-count.component';
 import { EditGoalComponent } from './edit-goal/edit-goal.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     ReactiveFormsModule,
-    DashboardPageRoutingModule
+    DashboardPageRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+   })
   ],
   declarations: [
     DashboardPage,
