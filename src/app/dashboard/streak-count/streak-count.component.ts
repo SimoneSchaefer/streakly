4f72f39591c1d9f6a71347a5ad87f87232fe0e97
Simone <simone@localhost.localdomain>
2020-12-17 22:03:24 +0100

    Display also record

--- src/app/dashboard/streak-count/streak-count.component.ts
+++ src/app/dashboard/streak-count/streak-count.component.ts
@@ -8,9 +8,21 @@ import { Component, Input, OnInit } from '@angular/core';
 export class StreakCountComponent implements OnInit {
 
   @Input() streakCount: number;
+  @Input() size: 'small' | 'large';
+  @Input() label: string = '';
 
   constructor() { }
 
   ngOnInit() {}
+
+  get color() {
+    if (this.streakCount < 10) {
+      return 'bronze';
+    }
+    if (this.streakCount < 20) {
+      return 'silver';
+    }
+    return 'gold';
+  }
 
 }
