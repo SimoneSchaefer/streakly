7a7b8f532ffa2507a13d8a9c4d11dbdd03f541e8
Simone <imkreislaufen@googlemail.com>
2020-12-20 13:35:25 +0100

    added unit tests for streak/dashboard

--- src/app/dashboard/streak-count/streak-count.component.spec.ts
+++ src/app/dashboard/streak-count/streak-count.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -25,4 +26,37 @@ describe('StreakCountComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('color change', () => {
+    it('should display bronze for streaks < 10', () => {
+      setCount(9);
+      expect(getMedal('bronze')).toBeTruthy();
+      expect(getMedal('silver')).toBeFalsy();
+      expect(getMedal('gold')).toBeFalsy();
+
+    });
+
+    it('should display silver for streaks < 20', () => {
+      setCount(19);
+      expect(getMedal('bronze')).toBeFalsy();
+      expect(getMedal('silver')).toBeTruthy();
+      expect(getMedal('gold')).toBeFalsy();
+    });
+
+    it('should display gold for streaks > 20', () => {
+      setCount(20);
+      expect(getMedal('bronze')).toBeFalsy();
+      expect(getMedal('silver')).toBeFalsy();
+      expect(getMedal('gold')).toBeTruthy();
+    });
+   });
+
+   function setCount(count: number) {
+     component.streakCount = count;
+     fixture.detectChanges();
+   }
+
+  function getMedal(color: string) {
+    return fixture.debugElement.query(By.css('.quiz-medal__circle--' + color));
+  }
 });
